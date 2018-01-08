10000d919a455b667b86a004862834c798bf6680
Roman-Admin <rcapsamun@yahoo.com>
2018-01-09 00:06:41 +0100

    add vehicles list

--- app/views.py
+++ app/views.py
@@ -1,7 +1,28 @@
 from flask import render_template, request
 from app import app
+from main import Database
+
+database = Database('main_amareto', 'michepass')
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title="General")
+    return render_template('index.html', title="General", acHome='active')
+
+
+@app.route('/vehicles')
+def vehicles():
+    sorted_vehicles = database.vehicles_col.find().sort([
+        ('max_load', 1)
+    ])
+    sorted_vehicles = [auto for auto in sorted_vehicles]
+    return render_template('vehicles.html',
+                           title='Vehicles',
+                           acVeh='active',
+                           vehicles=sorted_vehicles)
+
+
+@app.route('/products')
+def products():
+    return render_template('products.html', title='Products', acProd='active')
+
