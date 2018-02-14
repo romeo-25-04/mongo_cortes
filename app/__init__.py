6bd3d73ed430f6a47cedb00c57124a7dd3d974dc
lager6 <lager6@gmail.de>
2018-02-14 13:40:05 +0100

    add environmental vars support

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 app.config.from_object('config')
 if 'instance' in os.listdir() and 'config.py' in os.listdir('instance'):
     app.config.from_object('instance.config')
+if os.environ.get('MLAB_USER'):
+    app.config['MLAB_USER'] = os.environ.get('MLAB_USER')
+if os.environ.get('MLAB_PASSWORD'):
+    app.config['MLAB_PASSWORD'] = os.environ.get('MLAB_PASSWORD')
