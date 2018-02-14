6bbd947e86244757b1c9f8aa5d1f5db4d0c9862b
lager6 <lager6@gmail.de>
2018-02-14 13:06:18 +0100

    add instance test

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 app.config.from_object('config')
-app.config.from_object('instance.config')
+if 'instance' in os.listdir() and 'config.py' in os.listdir('instance'):
+    app.config.from_object('instance.config')
